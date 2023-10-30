17afaab6e010c15bd8f338cfaa4107e34e67ab75
Shayon Mukherjee <shayonj@users.noreply.github.com>
2023-10-29 21:51:53 -0400

    Eslint/prettier collab (#2)
    oof

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.status === "complete") {
     chrome.runtime.sendMessage({ type: "tabUpdated", tabId });
   }
