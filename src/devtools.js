259d4c6794e1e86bd80e133c4dc405ddb10d0421
Shayon Mukherjee <shayonj@gmail.com>
2023-10-29 22:36:21 -0400

    Add responses for sanitization, css/styling fix, 3 column layout

--- src/devtools.js
+++ src/devtools.js
@@ -60,9 +60,17 @@ function loadHARDetails() {
     const uniqueQueryParams = new Set();
 
     result.entries.forEach((entry) => {
+      // For request
       entry.request.headers.forEach((h) => uniqueHeaders.add(h.name));
       entry.request.cookies.forEach((c) => uniqueCookies.add(c.name));
       entry.request.queryString.forEach((q) => uniqueQueryParams.add(q.name));
+
+      if (entry.response) {
+        entry.response.headers.forEach((h) => uniqueHeaders.add(h.name));
+        if (entry.response.cookies) {
+          entry.response.cookies.forEach((c) => uniqueCookies.add(c.name));
+        }
+      }
     });
 
     displayData("headers-section", Array.from(uniqueHeaders));
@@ -75,10 +83,33 @@ function displayData(sectionId, data) {
   const section = document.getElementById(sectionId);
   section.innerHTML = "";
 
-  // Determine the number of columns, with a max of 4
-  const columnCount = Math.min(data.length, 4);
+  // Determine the number of columns, with a max of 3
+  const columnCount = Math.min(data.length, 2) + 1; // +1 for the "Select all" checkbox
   section.style.gridTemplateColumns = `repeat(${columnCount}, 1fr)`;
 
+  // Add "Select all" checkbox
+  const allItemDiv = document.createElement("div");
+  const allCheckbox = document.createElement("input");
+  allCheckbox.type = "checkbox";
+  allCheckbox.id = `${sectionId}-select-all`;
+
+  // Event listener for the "Check all" checkbox
+  allCheckbox.addEventListener("change", function () {
+    const otherCheckboxes = section.querySelectorAll(
+      'input[type="checkbox"]:not(#' + this.id + ")"
+    );
+    otherCheckboxes.forEach((checkbox) => (checkbox.checked = this.checked));
+  });
+
+  const allLabel = document.createElement("label");
+  allLabel.textContent = "Select all";
+  allLabel.setAttribute("for", allCheckbox.id);
+
+  allItemDiv.appendChild(allCheckbox);
+  allItemDiv.appendChild(allLabel);
+  section.appendChild(allItemDiv);
+
+  // Add the other checkboxes
   data.forEach((itemName, index) => {
     const itemDiv = document.createElement("div");
 
@@ -92,6 +123,20 @@ function displayData(sectionId, data) {
     const label = document.createElement("label");
     label.textContent = itemName;
     label.setAttribute("for", checkboxId);
+
+    // Event listener for individual checkboxes
+    checkbox.addEventListener("change", function () {
+      if (!this.checked) {
+        // If any individual checkbox is unchecked, uncheck the "Select all" checkbox
+        allCheckbox.checked = false;
+      } else if (
+        section.querySelectorAll('input[type="checkbox"]:not(:checked)')
+          .length === 1
+      ) {
+        // If all individual checkboxes are checked, check the "Select all" checkbox
+        allCheckbox.checked = true;
+      }
+    });
 
     itemDiv.appendChild(checkbox);
     itemDiv.appendChild(label);
